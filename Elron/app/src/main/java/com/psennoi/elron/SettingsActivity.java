97a5800d43555de34b1801605624daf4ec6775bf
Kristjan Pšennõi <kristjan.psennoi@hotmail.com>
2021-06-10 02:42:04 +0300

    settings ui done

--- Elron/app/src/main/java/com/psennoi/elron/SettingsActivity.java
+++ Elron/app/src/main/java/com/psennoi/elron/SettingsActivity.java
@@ -3,6 +3,7 @@ package com.psennoi.elron;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
 
 public class SettingsActivity extends AppCompatActivity {
 
@@ -10,5 +11,8 @@ public class SettingsActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_settings);
+    }
+
+    public void onTheme(View view) {
     }
 }
